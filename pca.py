b05ed1e875be4f1b0622332cab728401b51ea8a7
dan <dan.anderson174@gmail.com>
2023-01-13 11:48:27 +0000

    using dictionary over txt

--- pca.py
+++ pca.py
@@ -1,5 +1,7 @@
-import numpy as np
-with open("bin_to_coords.txt", "w") as out:
+import pickle
+
+def create_pickle_jar(): 
+    data = {}
     for i in range(2**18):
         binary = bin(i)[2:].zfill(18)
 
@@ -14,7 +16,17 @@ with open("bin_to_coords.txt", "w") as out:
         x = int(x_bin, 2)
         y = int(y_bin, 2)
 
-        x = ((x / (2 ** len(x_bin))) * 200)-100
-        y = ((y / (2 ** len(y_bin))) * 200)-100
+        x = ((x / (2 ** len(x_bin))) * 200) - 100
+        y = ((y / (2 ** len(y_bin))) * 200) - 100
+        data[binary] = (x, y)
         print(x,y)
-        out.write(str(binary) + ":" + "{:.2f}".format(x) + ":" + "{:.2f}".format(y) + "\n")
+
+    with open("data.pickle", "wb") as out:
+        pickle.dump(data, out)
+
+# create_pickle_jar()
+
+with open("data.pickle", "rb") as inp:
+    data = pickle.load(inp)
+
+print(data["111111111111101111"])
