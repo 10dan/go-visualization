78cec839e49a29d65e93e2848414bc027ecec16f
dan <dan.anderson174@gmail.com>
2023-01-16 14:21:13 +0000

    go legal moves funcs

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import pickle
 import helpers
 import copy
-from anytree import Node
 
-# TODO: FIND TREE LIBRARY that takes [[arrays]] not just strings.
-a = Node("test")
-b = Node("pig", parent=a)
-print(b)
 """
 Every GO board state will be represented in 3 states:
 - A 3x3 integer array
-    [ [0, 0, 1],
-      [0, 2, 0],
-      [1, 2, 0] ]
+    [ [0, 0, 1],      | 0 1 2
+      [0, 2, 0],      | 3 4 5
+      [1, 2, 0] ]     | 6 7 8
 - A 18 bit binary string
     |        x - coord        |        y - coord         |
     [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0]
@@ -38,9 +33,67 @@ def create_next_boards(board, parent_node):
                 new_board[x][y] = whos_turn
                 next_boards.append(new_board)
     print(next_boards)
+    
+def is_legal_move(board, x, y, player):
+    # Check if the move is within the board boundaries
+    if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
+        return False
+
+    # Check if the selected cell is not occupied
+    if board[x][y] != 0:
+        return False
+
+    # Check if the move captures any opponent's stones
+    opponent = 3 - player
+    captured = check_captures(board, x, y, opponent)
+
+    # Check if the move does not leave the player's own group in a captured state
+    return not check_captures(board, x, y, player)
+
+def check_captures(board, x, y, player):
+    captures = False
+
+    # Check in all four directions
+    for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+        if check_captures_direction(board, x, y, dx, dy, player):
+            captures = True
+
+    return captures
+
+def check_captures_direction(board, x, y, dx, dy, player):
+    captures = False
+
+    # Check if the next cell in the direction is occupied by the opponent
+    x2, y2 = x + dx, y + dy
+    if x2 < 0 or x2 >= len(board) or y2 < 0 or y2 >= len(board[0]) or board[x2][y2] != player:
+        return captures
+
+    # Check if the group has any liberties
+    liberties = check_group_liberties(board, x2, y2, player)
+    if liberties == 0:
+        captures = True
+    return captures
+
+def check_group_liberties(board, x, y, player):
+    liberties = 0
+    visited = set()
+    queue = [(x, y)]
+
+    while queue:
+        x, y = queue.pop(0)
+        visited.add((x, y))
+        for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+            x2, y2 = x + dx, y + dy
+            if x2 < 0 or x2 >= len(board) or y2 < 0 or y2 >= len(board[0]):
+                continue
+            if board[x2][y2] == 0:
+                liberties += 1
+            elif board[x2][y2] == player and (x2, y2) not in visited:
+                queue.append((x2, y2))
+    return liberties
 
 whos_turn = 1
 board = create_blank_board()
-root = Node(board)
+
 print(root)
 create_next_boards(board)
